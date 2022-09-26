77d208de5711bec7a820a16a574bdaf78e335a98
shohei <shohei.miyagi@devenues.com>
2022-09-26 13:52:37 +0900

    meal作成

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,57 @@
 class MealsController < ApplicationController
+    before_action :set_meal, only: [:show, :edit, :update, :delete]
+
+  # GET /meals
+  def index
+    @meals = Meal.all
+  end
+
+  # GET /meals/1
+  def show
+  end
+
+  # GET /meals/new
+  def new
+    @meal = Meal.new
+  end
+
+  # GET /meals/1/edit
+  def edit
+  end
+
+  # Meal /meals
+  def create
+    @meal = Meal.new(meal_params)
+
+    if @meal.save
+      redirect_to meal_path(@meal)
+    else
+      render :new
+    end
+  end
+
+  # PUT /meals/1
+  def update
+    if @meal.update(meal_params)
+      redirect_to meal_path(@meal)
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /meals/1
+  def delete
+    @meal.destroy
+    redirect_to meals_path
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_meal
+    @meal = Meal.find(params[:id])
+  end
+
+  def meal_params
+    params.require(:meal).permit(%i[title content])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Jets.application.routes.draw do
   resources :users
+  resources :meals
   resources :posts
   root "jets/public#show"
   any "*catchall", to: "jets/public#show"
