5c8541cbe217fbf496e3df13bdc72f86bb387968
shohei <shohei.miyagi@devenues.com>
2022-09-27 14:43:13 +0900

    material作成

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,5 @@ class Meal < ApplicationRecord
   belongs_to :user
   has_many :meal_materials
   has_many :materials, through: :meal_materials
-  mount_uploader :photo, PhotoUploader
+  # mount_uploader :photo, PhotoUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Jets.application.routes.draw do
   resources :users
   resources :meals
   resources :posts
+  resources :materials
   root "jets/public#show"
   any "*catchall", to: "jets/public#show"
 end
