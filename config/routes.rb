756c4c41190b750ea380f83be54b1749c84becba
shohei <shohei.miyagi@devenues.com>
2022-09-28 17:44:41 +0900

    meal編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Jets.application.routes.draw do
   resources :users
   resources :meals
-  resources :posts
   resources :materials
   root "jets/public#show"
   any "*catchall", to: "jets/public#show"
