c6b5e8c13b4814ddb54a0728a4d5571103fca78c
shohei <shohei.miyagi@devenues.com>
2022-09-29 16:22:17 +0900

    一旦コミット

--- db/migrate/20220925070346_create_meals.rb
+++ db/migrate/20220925070346_create_meals.rb
@@ -1,9 +1,9 @@
 class CreateMeals < ActiveRecord::Migration[6.1]
   def change
     create_table :meals do |t|
-      t.integer :user_id, null: false
+      t.integer :user_id
       t.string :title, null: false
-      t.string :photo, null: false
+      t.string :photo
       t.binary :make, null: false, limit: 3.gigabytes
       t.timestamps
     end
